aca5cbaabc796470f933d6f3faeabd876ffdab3e
DaniNapoli <63212529+DaniNapoli@users.noreply.github.com>
2021-06-12 23:24:31 -0300

    Velocidad en pasos por segundo

--- Codigo/app/src/main/java/com/example/tp2_grupo04/MainMenuActivity.java
+++ Codigo/app/src/main/java/com/example/tp2_grupo04/MainMenuActivity.java
@@ -2,12 +2,14 @@ package com.example.tp2_grupo04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -22,9 +24,14 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
 
 
 
+
     private Long previousTime;
 
     private Long actualTime;
+
+    private int previousSteps;
+
+    private int actualSteps;
 
     boolean running = false;
 
@@ -35,12 +42,12 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
 
         Intent intent2 = getIntent();
         Bundle extras = intent2.getExtras();
-        ;
 
         executeRefresh(extras.get("email").toString(), extras.get("password").toString(),
                         extras.get("token").toString(), extras.get("token_refresh").toString());
 
         previousTime=java.lang.System.currentTimeMillis();
+        previousSteps=0;
 
         tvSteps = (TextView) findViewById(R.id.textViewSteps2);
         tvSpeed = (TextView) findViewById(R.id.textViewSpeed2);
@@ -86,6 +93,7 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
         previousTime = java.lang.System.currentTimeMillis();
         running = true;
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        Log.i("pasos iniciales", String.valueOf(Sensor.TYPE_STEP_COUNTER));
         if(countSensor != null){
             sensorManager.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
         }else{
@@ -93,22 +101,30 @@ public class MainMenuActivity extends AppCompatActivity implements SensorEventLi
         }
     }
 
+    @SuppressLint("LongLogTag")
     @Override
     public void onSensorChanged(SensorEvent event){
         switch(event.sensor.getType()){
             case Sensor.TYPE_STEP_COUNTER:
                 if(running){
                     Long seconds;
+                    actualSteps= (int) event.values[0];
+                    Log.i("actual steps", String.valueOf(actualSteps));
+                    Log.i("previous steps", String.valueOf(previousSteps));
                     tvSteps.setText(String.valueOf(event.values[0]));
                     actualTime=java.lang.System.currentTimeMillis();
                     seconds=(actualTime-previousTime)/1000;
-                    if(seconds>=5){
+                    if(seconds>=10){
                         Log.i("previous Time",previousTime.toString());
                         Log.i("actual Time",actualTime.toString());
                         Log.i("seconds",seconds.toString());
                         //Log.i("total Time",totalTime.toString());
+
                         tvSpeed.setText(seconds.toString());
+                        tvSpeed.setText(String.valueOf((double)(actualSteps-previousSteps)/seconds)+"p/s");
+                        Log.i("actual steps - previous steps", String.valueOf(actualSteps-previousSteps));
                         previousTime=actualTime;
+                        previousSteps=actualSteps;
                     }
 
                 }
