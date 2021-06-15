ca121c7c915df9c754cd426f56f6015174294725
SantiagoAlbarracin <snalbarracin@live.com>
2021-06-15 17:38:39 -0300

    Toma Locacion

--- Codigo/app/src/main/java/com/example/tp2_grupo04/DiagnosisActivity.java
+++ Codigo/app/src/main/java/com/example/tp2_grupo04/DiagnosisActivity.java
@@ -1,9 +1,18 @@
 package com.example.tp2_grupo04;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,29 +20,56 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
+
 
 public class DiagnosisActivity extends AppCompatActivity {
 
     private JSONArray jsonArray;
     private HashMap<Integer, Hospital> hospitalsArray;
+    private Button btnLocation;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private Double lat;
+    private Double lon;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnosis);
+        btnLocation = (Button) findViewById(R.id.btnLocation);
+
+       fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
 
         generateHospitals();
         recorrerMap();
-
-
+        getLocation();
 
     }
 
+
+    private void getLocation(){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ){
+                if(getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if(location != null){
+                                lat = location.getLatitude();
+                                lon = location.getLongitude();
+                                Log.i("debug999", "Lat: " + lat.toString() + " Lon: " + lon.toString());
+                            }
+                        }
+                    });
+                }
+            }
+    }
+
     //Esta se va. Es para probar
-    public void recorrerMap(){
+    public void recorrerMap() {
         for (HashMap.Entry<Integer, Hospital> entry : hospitalsArray.entrySet()) {
             Integer key = entry.getKey();
             Hospital value = entry.getValue();
@@ -42,7 +78,7 @@ public class DiagnosisActivity extends AppCompatActivity {
     }
 
 
-    public void generateHospitals(){
+    public void generateHospitals() {
 
         JSONObject obj = null;
         Hospital hospital;
@@ -59,7 +95,7 @@ public class DiagnosisActivity extends AppCompatActivity {
                 String address = hsptProperties.getString("DOM_NORMA");
                 String telephone = hsptProperties.getString("TELEFONO");
                 JSONObject geometryArray = jo_inside.getJSONObject("geometry");
-                String coordinates =  geometryArray.getString("coordinates");
+                String coordinates = geometryArray.getString("coordinates");
                 hospital = new Hospital(name, address, telephone, coordinates);
                 hospitalsArray.put(i, hospital);
             }
@@ -69,9 +105,7 @@ public class DiagnosisActivity extends AppCompatActivity {
     }
 
 
-
-
-    public String readFile(){
+    public String readFile() {
         String json = null;
         try {
             InputStream is = this.getAssets().open("HospitalsBA");
@@ -85,6 +119,22 @@ public class DiagnosisActivity extends AppCompatActivity {
             return null;
         }
         return json;
+    }
+
+
+    public double calculateDistance(double lat1, double lng1, double lat2, double lng2) {
+
+        double earthRad = 6371;//en kilómetros
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lng2 - lng1);
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+        double va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+        double va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
+        double distance = earthRad * va2;
+
+        return distance;
     }
 
 
